76ce778b72b25f681d2ebef2ff09b8f7d7d9edc4
hare1925 <hare1925@163.com>
2019-10-27 21:26:05 +0000

    add some notes

---
+++ python/mooc/practice/6-3KeHeXueHuaV2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+# FileName : 科赫雪花
+# Author by : hare
+# Time : 2019.10.27
+
+#kochDrawV1.py
+import turtle as tt
+def koch(size, n):
+    if n == 0:
+        tt.fd(size)
+    else:
+        for angle in [0, 60, -120, 60]:
+            tt.left(angle)
+            koch(size/3, n-1)
+def main():
+    tt.setup(600, 600)
+    tt.penup()
+    tt.goto(-200, -100)
+    tt.pendown()
+    tt.pensize(2)
+    level = 3   #3阶科赫曲线，阶数
+    koch(400, level)    # 3阶科赫曲线，阶数
+    tt.right(120)
+    koch(400, level)
+    tt.right(120)
+    koch(400, level)
+    tt.hideturtle()
+main()
+
