5d66b82bdd83f8ac93e0e5dc891994f8b27f253f
hare1925 <hare1925@163.com>
2019-10-29 16:57:13 +0800

    add some notes

---
+++ python/cainiaolianxi/3python/23-maxGongYueshu.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# FileName : 最大公约数
+# Author by : hare
+# Time : 2019.10.29
+
+def hcf(x, y):
+    """该函数返回两个数的最大公约数"""
+
+    # 获取最小值
+
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+
+    for i in range(1, smaller + 1):
+        if ((x % i == 0) and (y % i == 0)):
+            hcf = i
+    return hcf
+
+# 用户输入两个数字
+num1 = int(input("输入第一个数字："))
+num2 = int(input("输入第二个数字："))
+
+print( num1, "和", num2, "的最大公约数为", hcf(num1, num2))
+
+
+
+
