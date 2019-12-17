8bff0f49372eafb34de38d208ebeceafb3b59084
hare1925 <hare1925@163.com>
2019-12-17 17:55:58 +0800

    rm some data

--- C/practice/1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	/*我的第一个 c 程序 */
-	printf("Hello, World! \n");
-	
-	return 0; 
-}
--- C/practice/6-support.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "6-main.c"
-
-extern int count;
-
-void write_extern(void)
-{
-	printf("count is %d\n", count);
-}
